ce8f7a671e212a4ea077c428a887042083050ed9
RANA YASHWANT SINGH <ranayashwantsingh@RANAs-Mac.local>
2026-06-22 19:46:02 +0530

    feat: add funds fetch and filter through api

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 import express from 'express';
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 // This is called "Middleware" - it allows your server to read JSON data
 app.use(express.json());
@@ -14,6 +14,31 @@ app.get('/api/health', (req, res) => {
     message: 'MF Screener API is running' 
   });
 });
+
+//for fetching and sorting/maping funds
+app.get('/api/funds', async (req, res) =>{
+  try{
+  const response = await fetch('https://api.mfapi.in/mf');
+  const data = await response.json();
+
+  const directFunds = data.filter(scheme =>
+    scheme.schemeName.includes("Direct")
+  );
+
+  const allScheme = directFunds.map(scheme => ({
+    code: scheme.schemeCode,
+    name: scheme.schemeName
+  }));
+
+    res.json(allScheme)
+  }
+
+  catch(error){
+  res.status(500).json(
+    {error: error.message});
+  }
+}
+);
 
 // This starts the server
 app.listen(PORT, () => {
