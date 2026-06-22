f76b520a6c537e5750737913577d2f0fe7a9fad2
RANA YASHWANT SINGH <ranayashwantsingh@RANAs-Mac.local>
2026-06-22 12:36:45 +0530

    feat: add fund fetch,filter and map code

---
+++ fetch-practice.js
@@ -0,0 +1,32 @@
+async function getcleanFunds() {
+    try{
+        const response = await fetch('https://api.mfapi.in/mf');
+        const data = await response.json();
+       
+        const allScheme = data.map(scheme => ({
+            code: scheme.schemeCode,
+            name: scheme.schemeName
+        })) ;
+
+ const directFunds = allScheme.filter
+        (scheme => scheme.name.includes("Direct")
+        );
+
+        const sortedDirectFundByName = directFunds.sort(
+           (a,b) => a.name.localeCompare(b.name)
+    );
+        
+    const top5Funds = sortedDirectFundByName.slice(0,8);
+        console.table(top5Funds);
+    }
+
+    
+    catch(error) {
+        console.error(error);
+    }
+}
+
+getcleanFunds();
+
+
+     
