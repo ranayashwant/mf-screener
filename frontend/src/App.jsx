d774f1bd3c9feee55885e92cd5905a2682c225ba
RANA YASHWANT SINGH <yashwant2609@gmail.com>
2026-06-26 19:08:59 +0530

    feat : added search feature in ui and linked it to database

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import {useState, useEffect} from 'react';
 
 function App() {
   //state variables
-  const[funds, setFunds] = useState([]); // empty array to store fetched funds
+  const[allFunds, setAllFunds] = useState([]); // empty array to store fetched funds
+  const [searchTerm, setSearchTerm] = useState(''); 
   const [loading, setLoading] = useState(true); //to show "loading..."
 
 
@@ -10,27 +11,50 @@ function App() {
   useEffect(() => {
     async function fetchFunds() {
       try {
+        
+
+        
+
         //backend is called for MFAPI data
         const response = await fetch('http://localhost:3000/api/funds');
         const data = await response.json();
-        setFunds(data); //set the fetched funds to state => screen re-renders with new data
+         setAllFunds(data); // Save the big list here
         setLoading(false);
       } catch (error) {
         console.error('Failed to fetch :', error);
+        setLoading(false);
         
       }
     }
     fetchFunds();
   }, []);    //empty array means run only once when component mounts
+
+  //for filtering from the fetched funds according to search term
+  const filteredFunds = allFunds.filter(fund =>
+    fund.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
     //if loading is true, show loading message
   if (loading) return <div className="p-8 text-xl">Loading funds...</div>;
 
     return (
-    <div className="p-8">
+    <div className="p-8 max-w-4xl mx-auto">
       <h1 className="text-3xl font-bold mb-6">Mutual Funds Screener</h1>
+       
+       {/*search input field to set searchTerm state variable*/}
+       <input type="text"
+       placeholder="Search by fund name"
+       className = "w-full p-3 border rounded-md shadow-sm mb-6 focus:outline-none focus:ring-3 focus:ring-blue-500"
+        value={searchTerm}
+
+        onChange={(e) => setSearchTerm(e.target.value)}
+       />
+
+        <p className="text-sm text-gray-500 mb-4">Showing {filteredFunds.length} of {allFunds.length} funds</p>
+
         {/*mapping acquired funds according to name and code */}
       <div className="grid gap-4:">
-        {funds.map(fund => (
+        {filteredFunds.map(fund => (
           <div 
           key ={fund.code}
           className="border p-4 rounded shadow hover : bg-gray-50"
