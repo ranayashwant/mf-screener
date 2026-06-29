7c539f486bc9406acac956f46457d17f5a52c559
RANA YASHWANT SINGH <yashwant2609@gmail.com>
2026-06-29 21:27:10 +0530

    fix: fix the database bug in railway

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,26 +1,20 @@
 import mysql from 'mysql2/promise';
 
-
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST || 'localhost',
+  port: process.env.MYSQLPORT || 3306,
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '',
+  database: process.env.MYSQLDATABASE || 'mf_screener',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-console.log("MYSQLHOST:", process.env.MYSQLHOST);
-console.log("MYSQLPORT:", process.env.MYSQLPORT);
-console.log("MYSQLUSER:", process.env.MYSQLUSER);
-console.log("MYSQLDATABASE:", process.env.MYSQLDATABASE);
-
 try {
   const conn = await pool.getConnection();
   console.log("✅ Connected to MySQL Database");
-  conn.release(); 
+  conn.release();
 } catch(error) {
   console.error("❌ Database connection failed:", error.message);
   console.error(error);
