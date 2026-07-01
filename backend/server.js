5980c59e4bb766996589b27cf68dad1fb0c90978
RANA YASHWANT SINGH <yashwant2609@gmail.com>
2026-07-01 15:24:17 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,6 @@ app.get('/api/health', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
